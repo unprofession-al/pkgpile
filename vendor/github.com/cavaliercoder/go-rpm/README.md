51224a12882cab4a3aa460d3cf0995317634f8e4
Daniel Menet <membership@sontags.ch>
2017-06-09 07:59:43 +0200

    added dependency mgmt

---
+++ vendor/github.com/cavaliercoder/go-rpm/README.md
@@ -0,0 +1,65 @@
+# go-rpm [![Build Status](https://travis-ci.org/cavaliercoder/go-rpm.svg?branch=master)](https://travis-ci.org/cavaliercoder/go-rpm) [![GoDoc](https://godoc.org/github.com/cavaliercoder/go-rpm?status.svg)](https://godoc.org/github.com/cavaliercoder/go-rpm)
+
+A native implementation of the RPM file specification in Go.
+
+	$ go get github.com/cavaliercoder/go-rpm
+
+
+The go-rpm package aims to enable cross-platform tooling for yum/dnf/rpm
+written in Go (E.g. [y10k](https://github.com/cavaliercoder/y10k)).
+
+Initial goals include like-for-like implementation of existing rpm ecosystem
+features such as:
+
+* Reading of modern and legacy rpm package file formats
+* Reading, creating and updating modern and legacy yum repository metadata
+* Reading of the rpm database
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/cavaliercoder/go-rpm"
+)
+
+func main() {
+	p, err := rpm.OpenPackageFile("golang-1.6.3-2.el7.rpm")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Loaded package: %v - %s\n", p, p.Summary())
+
+	// Output: golang-0:1.6.3-2.el7.x86_64 - The Go Programming Language
+}
+```
+
+
+## License
+Copyright (c) 2017 Ryan Armstrong. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ vendor/github.com/cavaliercoder/go-rpm/packagefile_test.go
@@ -0,0 +1,189 @@
+package rpm
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+var testFiles map[string][]byte
+
+func getTestFiles() map[string][]byte {
+	if testFiles != nil {
+		return testFiles
+	}
+
+	// get a directory full of rpms from RPM_DIR environment variable or
+	// failback to ./testdata
+	path := os.Getenv("RPM_DIR")
+	if path == "" {
+		path = "testdata"
+	}
+
+	// list RPM files
+	dir, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	files := make([]string, 0)
+	for _, f := range dir {
+		if strings.HasSuffix(f.Name(), ".rpm") {
+			files = append(files, filepath.Join(path, f.Name()))
+		}
+	}
+
+	if len(files) == 0 {
+		panic("No rpm packages found for testing")
+	}
+
+	testFiles = make(map[string][]byte, len(files))
+	for _, filename := range files {
+		b, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		testFiles[filename] = b
+	}
+
+	return testFiles
+}
+
+func TestReadRPMFile(t *testing.T) {
+	// load package file paths
+	files := getTestFiles()
+
+	valid := 0
+	for path, b := range files {
+		// Load package info
+		rpm, err := ReadPackageFile(bytes.NewReader(b))
+		if err != nil {
+			t.Errorf("Error loading RPM file %s: %s", path, err)
+		} else {
+			t.Logf("Loaded package: %v", rpm)
+			valid++
+		}
+	}
+
+	t.Logf("Validated %d RPM files", valid)
+}
+
+func TestReadRPMDirectory(t *testing.T) {
+	expected := 10
+	packages, err := OpenPackageFiles("./testdata")
+	if err != nil {
+		t.Fatalf("Error reading RPMs in directory: %v", err)
+	}
+
+	// count packages
+	if len(packages) != expected {
+		t.Errorf("Expected %d packages in directory; got %d", expected, len(packages))
+	}
+}
+
+func TestChecksum(t *testing.T) {
+	path := "./testdata/epel-release-7-5.noarch.rpm"
+	expected := "d6f332ed157de1d42058ec785b392a1cc4b5836c27830af8fbf083cce29ef0ab"
+
+	p, err := OpenPackageFile(path)
+	if err != nil {
+		t.Fatalf("Error opening %s: %v", path, err)
+	}
+
+	sum, err := p.Checksum()
+	if err != nil {
+		t.Errorf("Error validating checksum for %s: %v", path, err)
+	} else {
+		if sum != expected {
+			t.Errorf("Expected sum %s for %s; got %s", expected, path, sum)
+		}
+	}
+}
+
+func TestPackageFiles(t *testing.T) {
+	names := []string{
+		"/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7",
+		"/etc/yum.repos.d/epel-testing.repo",
+		"/etc/yum.repos.d/epel.repo",
+		"/usr/lib/rpm/macros.d/macros.epel",
+		"/usr/lib/systemd/system-preset/90-epel.preset",
+		"/usr/share/doc/epel-release-7",
+		"/usr/share/doc/epel-release-7/GPL",
+	}
+	modes := []int64{0644, 0644, 0644, 0644, 0644, 0755, 0644}
+	sizes := []int64{1662, 1056, 957, 41, 2813, 4096, 18385}
+	owners := []string{"root", "root", "root", "root", "root", "root", "root"}
+	groups := []string{"root", "root", "root", "root", "root", "root", "root"}
+	modtimes := []time.Time{
+		time.Unix(1416932629, 0),
+		time.Unix(1416932629, 0),
+		time.Unix(1416932629, 0),
+		time.Unix(1416932629, 0),
+		time.Unix(1416932629, 0),
+		time.Unix(1416932778, 0),
+		time.Unix(1416932629, 0),
+	}
+
+	path := "./testdata/epel-release-7-5.noarch.rpm"
+
+	p, err := OpenPackageFile(path)
+	if err != nil {
+		t.Fatalf("Error opening %s: %v", path, err)
+	}
+
+	files := p.Files()
+	if len(files) != len(names) {
+		t.Fatalf("expected %v files in RPM package but got %v", len(names), len(files))
+	}
+
+	for i, fi := range files {
+		name := fi.Name()
+		if name != names[i] {
+			t.Errorf("expected file %v with name %v but got %v", i, names[i], name)
+			continue
+		}
+
+		if mode := int64(fi.Mode().Perm()); mode != modes[i] {
+			t.Errorf("expected mode %v but got %v for %v", modes[i], mode, name)
+		}
+
+		if size := fi.Size(); size != sizes[i] {
+			t.Errorf("expected size %v but got %v for %v", sizes[i], size, name)
+		}
+
+		if owner := fi.Owner(); owner != owners[i] {
+			t.Errorf("expected owner %v but got %v for %v", owners[i], owner, name)
+		}
+
+		if group := fi.Group(); group != groups[i] {
+			t.Errorf("expected group %v but got %v for %v", groups[i], group, name)
+		}
+
+		if modtime := fi.ModTime(); modtime != modtimes[i] {
+			t.Errorf("expected modtime %v but got %v for %v", modtimes[i], modtime.Unix(), name)
+		}
+	}
+}
+
+func BenchmarkPackageOpens(b *testing.B) {
+	files := getTestFiles()
+	// parse packages from byte arrays b.N times
+	var V interface{}
+	for n := 0; n < b.N; n++ {
+		for _, b := range files {
+			p, err := ReadPackageFile(bytes.NewReader(b))
+			if err != nil {
+				panic(err)
+			}
+
+			V = p
+		}
+
+		X = V
+	}
+}
